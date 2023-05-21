5f4ed0e3b7dcd7f18fe3181f9b7d8f4bae1841f0
Riley Weaver <riley.weaver2006@gmail.com>
2023-05-20 17:24:13 -0700

    Start page and team selection page, buttons go back and forth

---
+++ app/src/main/java/com/example/scoutingappepiccoolsteamworks/SelectTeam.kt
@@ -0,0 +1,70 @@
+package com.example.scoutingappepiccoolsteamworks
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.scoutingappepiccoolsteamworks.ui.theme.ScoutingAppEpicCoolSteamworksTheme
+
+class SelectTeam : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.team_select)
+            val bRed1 = findViewById<Button>(R.id.red1)
+            bRed1.setOnClickListener {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            val bRed2 = findViewById<Button>(R.id.red2)
+            bRed2.setOnClickListener {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            val bRed3 = findViewById<Button>(R.id.red3)
+            bRed3.setOnClickListener {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            val bBlue1 = findViewById<Button>(R.id.blue1)
+            bBlue1.setOnClickListener {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            val bBlue2 = findViewById<Button>(R.id.blue2)
+            bBlue2.setOnClickListener {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            val bBlue3 = findViewById<Button>(R.id.blue3)
+            bBlue3.setOnClickListener {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+        }
+    }
+
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    ScoutingAppEpicCoolSteamworksTheme {
+        Greeting2("Android")
+    }
+}
